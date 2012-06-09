9faf32fd1e8ddb98a47ef1d194a7c2e86444e074
John P. Bloch <john@avendimedia.com>
2012-06-09 09:33:18 -0400

    Add hasher functionality and form display to the controller

--- Controller.php
+++ Controller.php
@@ -4,17 +4,59 @@ namespace Verily;
 
 class Controller extends \Core\Controller {
 
+	/**
+	 * The password hasher to encrypt passwords.
+	 * @var \Verily\Lib\PasswordHasher
+	 */
+	protected $hasher;
+
+	/**
+	 * A login form
+	 * @var \Core\Form
+	 */
+	protected $form;
+
+	/**
+	 * The display-able content
+	 * @var \Verily\View
+	 */
+	public $content;
+
 	public function initialize() {
+		$this->dependencies();
+	}
+
+	protected function dependencies() {
 		$globalConfig = config();
 		if( empty( $globalConfig->verilyIsInstalled ) ) {
 			$message = 'Verily is not installed! Please follow the instructions in INSTALL.txt!';
 			log_message( $message );
 			throw new \Exception( $message, 'not-installed' );
 		}
+		$passwordHasher = config( 'Verily' )->hasher;
+		if( !in_array( 'PasswordHasher', class_implements( $passwordHasher ) ) ) {
+			$passwordHasher = 'Lib\PasswordHash';
+		}
+		if( !($passwordHasher instanceof Lib\PasswordHasher) ) {
+			$passwordHasher = new $passwordHasher( config( 'Verily' )->hasher_iterations, config( 'Verily' )->use_portable_hashes );
+		}
+		$this->hasher = $passwordHasher;
 	}
 
 	public function run() {
 		
+	}
+
+	public function form( \Core\Validation $validation = null ) {
+		if( !$validation )
+			$validation = new \Core\Validation( array( ) );
+		$this->form = new \Core\Form( $validation );
+		$this->form->name->input( 'text' )->label( 'Name' )->wrap( 'p' );
+		$this->form->email->input( 'email' )->label( 'Email' )->wrap( 'p' );
+		$this->form->rememberMe->input( 'checkbox' )->value( '1' )->label( 'Remember Me' );
+		$this->form->submit->input( 'submit' )->value( 'Log In' )->wrap( 'p' );
+		$redirectTo = get( 'redirect_to', DOMAIN . DS . PATH, true );
+		$this->form->redirectTo->input( 'hidden' )->value( $redirectTo );
 	}
 
 	public function log_in() {
