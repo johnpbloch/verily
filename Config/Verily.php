d10a346fa2f6ff105d2517526bb6e1853f5c75a5
John P. Bloch <john@avendimedia.com>
2012-06-11 18:32:05 -0400

    More config options

--- Config/Verily.php
+++ Config/Verily.php
@@ -4,5 +4,9 @@ $config = array(
 	'hasher' => '\Verily\Lib\PasswordHash',
 	'hasher_iterations' => 8,
 	'use_portable_hashes' => true,
-	'default_form_view' => 'Form'
+	'default_form_view' => 'Form',
+	'user_model' => '',
+	'username_property' => 'username',
+	'password_property' => 'password',
+	'auth_salt' => '',
 );
