b5e28edb5a405f92a8b86dfaf33630db5e0c4843
John P. Bloch <john@avendimedia.com>
2012-06-15 17:30:40 -0400

    Remove the Lib/User.php file and transfer its contents to the Verily library class

--- Verily.php
+++ Verily.php
@@ -11,6 +11,16 @@ class Verily
 	 */
 	protected static $service;
 
+	/**
+	 * @var bool
+	 */
+	protected static $logged_in = null;
+
+	/**
+	 * @var \Core\ORM
+	 */
+	protected static $current_user;
+
 	protected static function maybeSetUp()
 	{
 		if( !self::$service )
@@ -36,6 +46,31 @@ class Verily
 					};
 			self::$service = $service;
 		}
+	}
+
+	protected static function set_auth_cookie( \Core\ORM $user, $remember = false )
+	{
+		self::$current_user = $user;
+		$userKey = $user->key();
+		$password_property = config( 'Verily' )->password_property;
+		$userPass = substr( (string)$user->{$password_property}, 8, 4 );
+		$expiration = time() + ( 86400 * ( $remember ? 14 : 1 ) );
+		$key = hash_hmac( 'md5', $userKey . $userPass . '|' . $expiration, config( 'Verily' )->auth_salt );
+		$hash = hash_hmac( 'md5', $userKey . $userPass . '|' . $expiration, $key );
+		$logged_in_data = array(
+			'user' => $userKey,
+			'hash' => $hash,
+			'expiration' => $expiration,
+		);
+		\Core\Cookie::set( 'verilyAuth', $logged_in_data );
+		self::$logged_in = true;
+	}
+
+	protected static function clear_auth_cookie()
+	{
+		self::$current_user = null;
+		self::$logged_in = false;
+		\Core\Cookie::set( 'verilyAuth', false );
 	}
 
 	/**
@@ -103,7 +138,7 @@ class Verily
 				$stored_hash = $user->{$passwordField};
 				if( self::$service->hasher()->CheckPassword( $data['password'], $stored_hash ) )
 				{
-					\Verily\Lib\User::set_auth_cookie( $user, post( 'rememberMe', false ) );
+					self::set_auth_cookie( $user, post( 'rememberMe', false ) );
 					redirect( post( 'redirectTo', DOMAIN, true ) );
 					exit;
 				}
@@ -120,8 +155,77 @@ class Verily
 	 */
 	public function log_out()
 	{
-		\Verily\Lib\User::clear_auth_cookie();
+		self::clear_auth_cookie();
 		return 'Successfully logged out!';
+	}
+
+	/**
+	 * Retrieve the current logged in user, if any
+	 *
+	 * @return \Core\ORM|bool The user object if logged in, false if not logged in
+	 */
+	public static function current_user()
+	{
+		return self::is_logged_in() ? self::$current_user : false;
+	}
+
+	/**
+	 * Check whether the user is logged in or not.
+	 *
+	 * @return boolean 
+	 */
+	public static function is_logged_in()
+	{
+		if( !is_null( self::$logged_in ) )
+		{
+			return (bool)self::$logged_in;
+		}
+		$data = \Core\Cookie::get( 'verilyAuth' );
+		if( !$data )
+		{
+			self::$logged_in = false;
+			self::$current_user = null;
+			return false;
+		}
+		extract( $data );
+		if( $expiration < time() )
+		{
+			self::clear_auth_cookie();
+			return false;
+		}
+		$userModel = config( 'Verily' )->user_model;
+		$user = new $userModel( $user );
+		if( !$user->key() )
+		{
+			self::clear_auth_cookie();
+			return false;
+		}
+		$passwordProp = config( 'Verily' )->password_property;
+		$pass_fragment = substr( $user->{$passwordProp}, 8, 4 );
+		$blob = $user->key() . $pass_fragment . '|' . $expiration;
+		$key = hash_hmac( 'md5', $blob, config( 'Verily' )->auth_salt );
+		$testHash = hash_hmac( 'md5', $blob, $key );
+		if( $testHash === $hash )
+		{
+			self::$logged_in = true;
+			if( !self::$current_user )
+			{
+				self::$current_user = $user;
+			}
+			return true;
+		}
+		self::clear_auth_cookie();
+		return false;
+	}
+
+	/**
+	 * Hash a password using the same method Verily will use to authenticate.
+	 * @param string $password
+	 * @return string The hashed password 
+	 */
+	public static function hashPassword( $password )
+	{
+		return self::$service->hasher()->HashPassword( $password );
 	}
 
 }
